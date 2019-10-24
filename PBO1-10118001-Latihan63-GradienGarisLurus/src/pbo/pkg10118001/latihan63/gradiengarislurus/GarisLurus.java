1282e653f1f3ab78e6f0afa32194d6bdc2a4a179
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-10-24 23:55:02 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan63-GradienGarisLurus/src/pbo/pkg10118001/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118001.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi :
+ */
+public interface GarisLurus {
+    
+    public int hitungGradien();
+    
+}
